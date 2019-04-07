5613dfd5795aec3e699e191f48000940b23fd1a1
JamesMcGuigan <james.mcguigan.github@gmail.com>
2019-04-07 16:55:32 -0700

    main.py | test using ProcessPool over ThreadPool

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import time
-from multiprocessing import Queue, Manager
+from multiprocessing import Manager
 
 from glob2 import glob
 
@@ -9,20 +9,39 @@ from src.util.IterableQueue import IterableQueue
 
 
 def main():
-    queue     = Queue()
+    print "START - main()"
+
     manager   = Manager()
+    queue     = manager.Queue()  # ThreadPool() works with Queue.Queue(); ProcessPool requires Autoproxy: manager.Queue()
     filenames = glob('./data/occupancy_data/*.txt')
+    processes = []
 
     def start_reader(filename):
-        reader = CSVReader(filename, queue=queue, wrapper=dict)
-        reader.start()
+        print "START - start_reader(", queue, ")"
+        CSVReader(filename, queue=queue, wrapper=dict, start=True)
+        print "END   - start_reader(", queue, ")"
     # map(start_reader, filenames)
-    Globals().thread_pool.amap(start_reader, filenames)
+    processes += [ Globals().process_pool.amap(start_reader, filenames) ]
+
 
     def print_queue(queue):
-        print "print_queue(", queue, ")"
-        while queue.empty(): time.sleep(0.1)
-        for item in IterableQueue(queue):
-            print item
+        print "START - print_queue(", queue, queue.qsize(), ")"
+        while queue.empty():
+            time.sleep(1)
+        for count, item in enumerate(IterableQueue(queue)):
+            print queue.qsize(), item
+        print "END - print_queue(", queue, queue.qsize(), ")"
     # print_queue(queue)
-    Globals().thread_pool.apipe(print_queue, queue)
+    processes += [ Globals().process_pool.apipe(print_queue, queue) ]
+
+
+    ### TODO: Investigate - pathos doesn't cleanly exit process, or run final line of code
+    ### TODO: Investigate - Ctrl-C - onKeyboardInterupt = RuntimeError("cannot join current thread")
+    # Globals().process_pool.join()
+    # for process in processes: process.join()
+
+    print "END - main()"
+
+
+if __name__ == '__main__':
+    main()
