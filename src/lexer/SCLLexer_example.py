5d6c4cc41720e167febbb0229ce133a99f3c0197
JamesMcGuigan <james.mcguigan.github@gmail.com>
2019-04-12 18:53:59 -0700

    SCLLexer | rename SCLLexer_test.py -> SCLLexer_example.py

---
+++ src/lexer/SCLLexer_example.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python2
+
+import re
+from os import path
+
+import simplejson as json
+
+from src.lexer.SCLLexer import SCLLexer
+
+
+
+if __name__ == '__main__':
+    ### Example: https://www.dabeaz.com/ply/ply.html#ply_nn4
+    
+    _filename = path.abspath(__file__)
+    _dirname  = path.dirname(_filename)
+    datafile = path.join( _dirname, 'SCL.txt' )
+
+    with open(datafile) as file:
+        data_lines = file.readlines()
+        lexer  = SCLLexer()
+        for linenumber, line in enumerate(data_lines):
+            if not re.match(r'^\s*$', line):
+                print "\n**********\n", linenumber, ':', line, "**********\n"
+
+                if hasattr(lexer, 'lexer'):
+                    lexer.lexer.input(line)
+                    while True:
+                        token = lexer.lexer.token()
+                        if not token: break # No more input
+                        print token
+
+                if hasattr(lexer, 'parser'):
+                    result = lexer.parser.parse(line)
+                    print "***** result *****: "
+                    print json.dumps(result, indent=4*' ')
